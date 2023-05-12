8b01ef8e8259e056543fd4385fea3661c19cc55e
James Yang <james.yang92@gmail.com>
2023-05-11 21:31:56 -0700

    Added multiprocessing

--- analyze-multithread.py
+++ analyze-multithread.py
@@ -14,32 +14,19 @@ from typing import List
 def process_audio_file(
     audio_file : str,
     output_file_path : str,
-    tarball_path : str,
-    recompute_existing : bool
 ) -> None:
-    print(f"Processing {audio_file}")
-    os.makedirs(os.path.dirname(output_file_path), exist_ok=True)
-    if not recompute_existing and os.path.exists(output_file_path):
-        # print(f"{output_file_path} already exists. Skipping...")
-        return
     try:
-        # Extract file and write it directly to the temporary file.
-        ff = tempfile.NamedTemporaryFile(mode="wb", suffix=".wav")
-        with tarfile.open(tarball_path, "r") as src_tar:
-            audio_tar_member = src_tar.getmember(audio_file)
-            audio_file = src_tar.extractfile(audio_tar_member)
-            ff.write(audio_file.read())
-        analyze.analyze(ff.name, output_file_path, True, True, True, "json", None)
+        analyze.analyze(audio_file, output_file_path, True, True, True, "json", None)
     except Exception as e:
         print(f"Couldn't process file {audio_file}. Reason: {str(e)}. Skipping...")
     finally:
         print(f"Created {output_file_path}")
-        ff.close()
 
 def process_tarball(tarball_path : str, recompute_existing : bool) -> None:
     parent_dir_name = os.path.dirname(tarball_path).split("/")[-1]
     # Get all filenames in the tarball.
     with tarfile.open(tarball_path, "r") as src_tar:
+        # Grab all the filenames in the tarball and their designated output filenames.
         data_samples = src_tar.getnames()
         audio_files = [f for f in data_samples if f.endswith(".wav")]
         output_filenames = [
@@ -49,30 +36,35 @@ def process_tarball(tarball_path : str, recompute_existing : bool) -> None:
                 )
             ) for af_name in audio_files
         ]
-        for audio_file_name, output_file_path in zip(audio_files, output_filenames):
+        # This part is incredibly stupid, so please fix somehow.
+        # Since the analysis code uses essentia to parse sound files, there's no
+        # option to feed in file descriptors, only file paths. Here, we
+        # essentially untar every audio file, dump its contents into a temporary
+        # file, process that temporary file, and then delete the temporary file.
+        # Ironically, each file takes between 3s-15s to process, so the I/O is
+        # not a bottleneck compared to all the signal processing. This approach
+        # allows us to do parallel computations.
+        temp_files, pruned_out_files = list(), list()
+        for i, (audio_file_name, output_file_path) in enumerate(zip(audio_files, output_filenames)):
+            # If the output JSON file already exists, then skip.
             if not recompute_existing and os.path.exists(output_file_path):
                 print(f"{output_file_path} already exists. Skipping...")
                 continue
-            try:
-                # Extract file and write it directly to the temporary file.
-                ff = tempfile.NamedTemporaryFile(mode="wb", suffix=".wav")
-                audio_tar_member = src_tar.getmember(audio_file_name)
-                audio_file = src_tar.extractfile(audio_tar_member)
-                ff.write(audio_file.read())
-                analyze.analyze(ff.name, output_file_path, True, True, True, "json", None)
-            except Exception as e:
-                print(f"Couldn't process file {audio_file_name}. Reason: {str(e)}. Skipping...")
-            finally:
-                print(f"Created {output_file_path}")
-                ff.close()
+            # Create the output file directory if it doesn't already exist.
+            os.makedirs(os.path.dirname(output_file_path), exist_ok=True)
+            # Extract file and write it directly to the temporary file.
+            temp_files.append(tempfile.NamedTemporaryFile(mode="wb", suffix=".wav"))
+            audio_tar_member = src_tar.getmember(audio_file_name)
+            audio_file = src_tar.extractfile(audio_tar_member)
+            temp_files[-1].write(audio_file.read())
+            pruned_out_files.append(output_file_path)
 
-    # Create a processing job for each filename.
-    # jobs = [
-    #     (audio_file, output_filename, tarball_path, recompute_existing)
-    #     for audio_file, output_filename in zip(audio_files, output_filenames)
-    # ]
-    # with multiprocessing.Pool(multiprocessing.cpu_count()) as pool:
-    #     pool.starmap(process_audio_file, jobs)
+        temp_fnames = [temp_file.name for temp_file in temp_files]
+        with multiprocessing.Pool(processes=multiprocessing.cpu_count()) as pool:
+            pool.starmap(process_audio_file, zip(temp_fnames, pruned_out_files))
+
+        for temp_file in temp_files:
+            temp_file.close()
     
 def get_all_tarball_paths(in_path : str) -> List[str]:
     tarball_fullpaths = list()
